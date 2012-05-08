0fdd5b709a9eb4e5b27d6a054fcdc9c58f372a6f
C0deAttack <viru@Viru-Doshis-iMac.local>
2012-05-08 21:59:02 +0100

    Updated to Cucumber JVM 1.0.6

--- src/test/java/com/c0deattack/cucumberspring/AppTest.java
+++ src/test/java/com/c0deattack/cucumberspring/AppTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(features = "features/", format = { "pretty", "html:target/cucumber" })
+@Cucumber.Options(features = "classpath:features", format = { "pretty", "html:target/cucumber" })
 public class AppTest {
 
 }
